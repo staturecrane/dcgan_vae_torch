3d11d93c77a34f46fc2bc4301a87ac807a778ec5
Richard Herbert <richard.alan.herbert@gmail.com>
2016-10-18 16:58:52 +0000

    discriminator model

---
+++ discriminator.lua
@@ -0,0 +1,24 @@
+require 'nn'
+
+local discriminator = {}
+
+function discriminator.get_discriminator(channels, ndf)
+    netD = nn.Sequential()
+    netD:add(nn.SpatialConvolution(channels, ndf, 4, 4, 2, 2, 1, 1))
+    netD:add(nn.LeakyReLU(0.2, true))
+    netD:add(nn.SpatialConvolution(ndf, ndf * 2, 4, 4, 2, 2, 1, 1))
+    netD:add(nn.SpatialBatchNormalization(ndf * 2)):add(nn.LeakyReLU(0.2, true))
+    netD:add(nn.SpatialConvolution(ndf * 2, ndf * 4, 4, 4, 2, 2, 1, 1))
+    netD:add(nn.SpatialBatchNormalization(ndf * 4)):add(nn.LeakyReLU(0.2, true))
+    netD:add(nn.SpatialConvolution(ndf * 4, ndf * 8, 4, 4, 2, 2, 1, 1))
+    netD:add(nn.SpatialBatchNormalization(ndf * 8)):add(nn.LeakyReLU(0.2, true))
+    netD:add(nn.SpatialConvolution(ndf * 8, ndf * 16, 4, 4, 2, 2, 1, 1))
+    netD:add(nn.SpatialBatchNormalization(ndf * 16)):add(nn.LeakyReLU(0.2, true))
+    netD:add(nn.SpatialConvolution(ndf * 16, 1, 4, 4))
+    netD:add(nn.Sigmoid())
+    netD:add(nn.View(1):setNumInputDims(3))
+    
+    return netD
+end
+
+return discriminator
